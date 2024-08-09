0c6e9702473e77081a1b013f4b4dbbeb406a8a46
eKhafizov <130173250+eKhafizov@users.noreply.github.com>
2024-08-09 10:01:07 +0300

    started with rtk-query

---
+++ src/features/apiSlice.ts
@@ -0,0 +1,21 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { OffersArray } from '../types/types';
+import { APIRoute } from '../store/utils/apiRoutes';
+
+// Define a service using a base URL and expected endpoints
+export const sixSitiesApi = createApi({
+  //reducer name for store
+  reducerPath: 'sixCitiesApiReducer',
+  // base path
+  baseQuery: fetchBaseQuery({baseUrl: 'https://13.react.htmlacademy.pro/six-cities' }),
+  // all endpoint from our APIRoute
+  endpoints: (builder) => ({
+    getOffers: builder.query<OffersArray, string>({
+      query: () => APIRoute.Offers,
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const {useGetOffersQuery} = sixSitiesApi;
